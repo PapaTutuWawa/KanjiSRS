8d6b098cdd2a8c226a0ebe4d40804278289004e7
Alexander Polynomdivision <digitalmail555@googlemail.com>
2018-04-06 20:55:21 +0200

    fix(comp): Add the 'Props'-suffix

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 
 import { Route, Redirect } from "react-router-dom";
 
-interface IAuthRoute {
+interface IAuthRouteProps {
     path: string;
     component: any;
     isAuth: () => boolean;
 }
 
-export default class AuthRoute extends React.Component<IAuthRoute, {}> {
+export default class AuthRoute extends React.Component<IAuthRouteProps, {}> {
     render() {
         const auth = this.props.isAuth();
         return <Route path={this.props.path} component={
