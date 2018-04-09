c5e63c45fd24d37db2003f928d70a9c97c9d16fd
Alexander Polynomdivision <digitalmail555@googlemail.com>
2018-04-09 20:47:48 +0200

    fix(comp): Make the text appear white

--- src/components/VocabTableRow.tsx
+++ src/components/VocabTableRow.tsx
@@ -11,18 +11,17 @@ interface IVocabTableRowProps {
 
 export default class VocabTableRow extends React.Component<IVocabTableRowProps, {}> {
     render() {
-        // TODO: The vocabulary should appear white
         return <TableRow
                    style={{
-                       backgroundColor: ResultTypeColor(this.props.result.type)
+                       backgroundColor: ResultTypeColor(this.props.result.type),
                    }}>
             <TableCell>
-                <Typography variant="display1" color="inherit">
+                <Typography variant="display1" style={{ color: "white" }}>
                     { this.props.result.question.vocab.japanese }
                 </Typography>
             </TableCell>
             <TableCell>
-                <Typography variant="body1" color="inherit">
+                <Typography variant="body1" style={{ color: "white" }}>
                     { QuestionTypeString(this.props.result.question.type) }
                 </Typography>
             </TableCell>
